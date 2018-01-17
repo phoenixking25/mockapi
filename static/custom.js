b74838b56f88ac1ee129178a87e3a59e496c50e8
Anuj Sharma <anujjangra25119@gmail.com>
2018-01-17 23:36:16 +0530

    final without post and put

--- static/custom.js
+++ static/custom.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $("#add").click(function(){
         var count = $(".added").length;
         count += 2;
-        $("#insert").append(" <div class='row'><div class='input-field col s12'><input id='field' type='text' class='validate added' name='field" + count + "' required><label for='field'>Field " + count + "</label></div></div>");
+        $("#insert").append(" <div class='row'><div class='input-field col s6'><input id='field' type='text' class='validate added' name='field" + count + "' required><label for='field'>Field " + count + "</label></div><div class='input-field col s6'><select name='datatype" + count + "' required class='validate'><option value='' disabled selected>Choose your option</option><option value='1'>String</option><option value='2'>Date</option><option value='3'>Object</option><option value='4'>Boolean</option><option value='5'>Number</option><option value='6'>Array</option></select><label>Datatype Select</label></div></div>");
+        $(document).ready(function() {
+            $('select').material_select();
+        });
     });
 });
 
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import random
 
 app = Flask(__name__)
 
@@ -10,34 +11,28 @@ data = []
 def index():
     api_running = False
     if request.method == 'POST':
-        print len(request.form)
-        print request.form["field" + str(0+1)]
-        for i in xrange(len(request.form) - 2):
-            fields.append(str(request.form["field" + str(i+1)]))
+        for i in xrange((len(request.form) - 2)/2):
+            fields.append(dict(field_name = str(request.form["field" + str(i+1)]), datatype = int(request.form["datatype" + str(i+1)])))
         number = request.form['number']
         make_data(int(number))
         return render_template('form.html', api_running=True)
     return render_template('form.html')
 
-
-
-
 @app.route('/get', methods=['GET'])
 def get_request():
     return json.dumps(data), 200
 
-@app.route('/post', methods=['POST'])
-def post_request():
-    if request.method == 'POST':
-        global data
-        obj = dict(id = len(data) + 1, field1 = request.form[''])
-        data.append(obj)
-        return 'success', 200
+# @app.route('/post', methods=['POST'])
+# def post_request():
+#     if request.method == 'POST':
+#         global data
+#         # obj = dict(id = len(data) + 1, request.form)
+#         data.append(obj)
+#         #return 'success', 200
 
 @app.route('/get/<id>', methods=['GET'])
 def get_item(id):
     global data
-    print len(data)
     return json.dumps(data[int(id) - 1]), 200
 
 @app.route('/delete/<id>', methods=['DELETE'])
@@ -55,9 +50,19 @@ def make_data(number):
     obj = {}
     for i in xrange(number):
         obj['id'] = i + 1
-        print obj['id']
         for j in fields:
-            obj[j] = j + " " + str(i + 1)
+            if j["datatype"] == 1:
+                obj[j["field_name"]] = j["field_name"] + " " + str(i + 1)
+            elif j["datatype"] == 2:
+                obj[j["field_name"]] = random.randrange(1316211567000, 1916211567000)
+            elif j["datatype"] == 3:
+                obj[j["field_name"]] = {}
+            elif j["datatype"] == 4:
+                obj[j["field_name"]] = False
+            elif j["datatype"] == 5:
+                obj[j["field_name"]] = random.randrange(0, 101)
+            elif j["datatype"] == 6:
+                obj[j["field_name"]] = []
         data.append(obj)
         obj = {}
 
